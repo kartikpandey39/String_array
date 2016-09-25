aa7477856eef776c11c976641d6093b5bcba0f0b
kartikpandey39 <kartikpandey39@gmail.com>
2016-09-25 17:43:53 +0530

    Add files via upload
    You are given N strings of alphabet characters and the task is to find their matching decimal representation as on the shown keypad. Output the decimal representation corresponding to the string. For ex: if you are given “amazon” then its corresponding decimal representation will be 262966.
    
    Input:
    
    The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. Each test case consists of a single line containing a string.
    
    Output:
    
    For each test case, print in a new line, the corresponding decimal representation of the given string.
    
    Constraints:
    
    1 ≤ T ≤ 100
    1 ≤ length of String ≤ 100
    
    Example:
    
    Input
    2
    geeksforgeeks
    geeksquiz
    
    Output
    4335736743357
    433577849

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gfg_string_KeypadTyping
+{
+    class Keypad
+    {
+        // char[] c;
+        public string s;
+        public void GenerateCode()
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i]<=111)
+                {
+                    int c = (s[i] - 97) / 3;
+                    Console.Write(c + 2);
+                }
+                else
+                if (s[i]>111&& s[i]<=115)
+                {
+                    Console.Write('7');
+                }
+                else
+                if (s[i] > 115 && s[i] <= 118)
+                {
+                    Console.Write('8');
+                }
+                else
+                if (s[i] > 118 && s[i] <= 122)
+                {
+                    Console.Write('9');
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int test = int.Parse(Console.ReadLine());
+            Keypad[] k = new Keypad[test];
+            for (int i = 0; i < test; i++)
+            {
+                k[i] = new Keypad();
+                k[i].s = Console.ReadLine();
+            }
+            for (int i = 0; i < test; i++)
+            {
+                k[i].GenerateCode();
+            }
+            Console.Read();
+        }
+    }
+}
